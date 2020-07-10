d62724c65b89173f64e8a7af205f743a6ca4e183
Alexis Castellanos <intelaki@gmail.com>
2020-07-10 17:34:37 -0500

    'RutaAutenticada' updated

--- src/componentes/seguridad/RutaAutenticada.js
+++ src/componentes/seguridad/RutaAutenticada.js
@@ -1,3 +1,24 @@
-import React from 'react';
-import {Route,Redirect} from 'react-router-dom';
-import {useStateValue} from '../../sesion/store';
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { useStateValue } from "../../sesion/store";
+
+function RutaAutenticada({
+	compnent: Component,
+	autenticadoFirebase,
+	...rest
+}) {
+	const [{ autenticado }, dispatch] = useStateValue();
+	return (
+		<Route
+			{...rest}
+			render={(props) =>
+				autenticado === true || autenticadoFirebase !== null ? (
+					<Component {...props} {...rest} />
+				) : (
+					<Redirect to="/auth/login" />
+				)
+			}
+		/>
+	);
+}
+export default RutaAutenticada;
